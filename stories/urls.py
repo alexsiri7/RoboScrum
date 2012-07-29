a2c57498be00a75ad034e005584cae96c1af6acd
alexsiri7 <alexsiri7@gmail.com>
2012-07-29 12:11:08 +0100

    Targeted value increase in sprint view

--- stories/urls.py
+++ stories/urls.py
@@ -1,18 +1,13 @@
-from django.views.generic import DetailView, ListView
 from django.conf.urls.defaults import *
-from stories.models import Sprint
+from stories.views import SprintView, SprintListView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('stories.views',
-    url(r'^$',
-         ListView.as_view(
-		queryset=Sprint.objects.all().order_by('-start_date')[:5]
-	)),
+    url(r'^$',SprintListView.as_view()),
     url(r'^sprint/(?P<pk>\d+)/$',
-         DetailView.as_view(
-            model=Sprint)
+         SprintView.as_view()
 	),
 )
 
--- stories/views.py
+++ stories/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader
 from stories.models import Story, Sprint
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
 
-def home(req):
-    latest_sprint_list = Sprint.objects.all().order_by('-start_date')[:5]
-    c = {'latest_sprint_list': latest_sprint_list}
-    return render_to_response('sprint/index.html', c)
+class SprintView(DetailView):
+    days = ["Start", "Mon", "Tue", "Wed", "Thu", "Fri"]
+    model = Sprint
 
-def detail(req, sprint_id):
-    sprint = get_object_or_404(Sprint,id=sprint_id)
-    return render_to_response('sprint/detail.html',{'sprint': sprint})
+    def get_context_data(self, **kwargs):
+        context = super(SprintView, self).get_context_data(**kwargs)
+        context['burndown'] = self.burndown()
+        return context
+    def burndown(self):
+      total = self.object.original_commitment()
+      burn = map(lambda (i,e): (self.days[i], total-total*i/5, total*1.2-total*i/5*1.2, total*0.8-total*i/5*0.8,total-e),enumerate(self.object.burnup()))
+      return burn
+
+
+class SprintListView(ListView):
+    queryset = Sprint.objects.all().order_by('-start_date')
+    def get_context_data(self, **kwargs):
+        context = super(SprintListView, self).get_context_data(**kwargs)
+        context['TVI'] = self.getTVI()
+        return context
+    def getTVI(self):
+        return map(lambda s: (s.number, s.targeted_value_increase()), self.object_list.order_by('start_date').all())
